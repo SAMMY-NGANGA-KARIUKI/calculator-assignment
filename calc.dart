f3b8d2ebecdd6d792aebfa9f98b209e40e993768
sammy kariuki nganga <ngangasamuel180@gmail.com>
2023-09-07 16:27:51 +0300

    calculator assignment

---
+++ calc.dart
@@ -0,0 +1,14 @@
+import "dart:math";
+void main(){
+  int num1=70;
+  int num2=35;
+  var multiply=num1*num2;
+  var add=num1+num2;
+  var subtract=num1-num2;
+  var devide=num1/num2;
+  print(multiply);
+  print(add);
+  print(subtract);
+  print(devide);
+  print(sqrt(100));
+}
